4cd07b4fdf53c4ae256d5f176679f27ec2cbb758
Nick Harberg <nick@seamonsterstudios.com>
2014-05-07 21:21:55 -0700

    added package.json and Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	grunt.initConfig({
+		jshint: {
+			options: {
+				"curly": true,
+			  	"eqnull": true,
+			  	"eqeqeq": true,
+			  	"undef": true,
+			  	"globals": {
+			  		"jQuery": true
+			  	}
+			},
+			target1: ['Gruntfile.js', 'src/**/*.js']
+		}
+	});
+
+	grunt.registerTask('default', ['jshint']);
+};
